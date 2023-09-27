4d817ebde37c98f5d9796ccf1501d2a41eb454bf
VinSalus <viniciusmsalustiano@hotmail.com>
2023-09-27 15:21:23 -0300

    First comit

---
+++ shopcart/src/test/java/com/shopcartproject/shopcart/ShopcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopcartproject.shopcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
